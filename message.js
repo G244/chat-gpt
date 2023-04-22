5545ab2a8c8b412d325da31da6d35c5934dfa088
G244 <58899275+G244@users.noreply.github.com>
2023-04-22 22:44:46 +0800

    Update message.js

--- message.js
+++ message.js
@@ -159,8 +159,8 @@ export class Message {
 
     /*passive message*/
     reply(res, options, toUser) {
-        console.log('reply')
-        res.writeHead(200, { 'Content-Type': 'application/xml' });
+        console.log('reply');
+       
         var resMsg = xmlmsg1(toUser, process.env.CORPID, this.timestamp(), options.content);
 
         const msgEncrypt = this.encryptMsg(resMsg);
